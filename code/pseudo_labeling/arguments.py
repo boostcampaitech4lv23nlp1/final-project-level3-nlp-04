346f7166fb362bc887cc51ff18578fbc38119ca8
JunHyung1206 <pjh120610@naver.com>
2023-01-15 06:44:38 +0000

    Fix: change default value

--- code/pseudo_labeling/arguments.py
+++ code/pseudo_labeling/arguments.py
@@ -14,34 +14,33 @@ class Arguments:
             "help": "Path to pretrained model or model identifier from huggingface.co/models"
         },
     )
-    
+
     train_data: Optional[str] = field(
-        default="./emotion_all_20_train.csv",
+        default=None,
         metadata={"help": "The name of the dataset to use."},
     )
-    
+
     eval_data: Optional[str] = field(
-        default="./emotion_all_20_eval.csv",
+        default=None,
         metadata={"help": "The name of the dataset to use."},
     )
-    
-    
+
     max_seq_length: int = field(
-        default=500,
+        default=512,
         metadata={
             "help": "The maximum total input sequence length after tokenization. Sequences longer "
             "than this will be truncated, sequences shorter will be padded."
         },
     )
-    
+
     wandb_project: str = field(
         default="None_project",
     )
-    
+
     wandb_entity: Optional[str] = field(
         default="sajo-tuna",
     )
-    
+
     wandb_name: str = field(
         default="None_name",
     )
