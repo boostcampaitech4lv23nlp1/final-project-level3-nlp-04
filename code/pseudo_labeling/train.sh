1cf616d1e9956ba429ce84505385680aee619f66
JunHyung1206 <pjh120610@naver.com>
2023-01-15 06:42:44 +0000

    Fix: del quotes

--- code/pseudo_labeling/train.sh
+++ code/pseudo_labeling/train.sh
@@ -1,23 +1,22 @@
 python train.py \
 --output_dir ./models/train_dataset \
---do_train \
---do_eval \
 --fp16 \
 --overwrite_output_dir \
---train_data "./emotion_all_20_train.csv" \
---eval_data "./emotion_all_20_eval.csv" \
---model_name_or_path "klue/roberta-large" \
---per_device_train_batch_size 8 \
---per_device_eval_batch_size 8 \
---gradient_accumulation_steps 1 \
---num_train_epochs 1 \
+--train_data ./data/emotion_all_20_train.csv \
+--eval_data ./data/emotion_all_20_eval.csv \
+--model_name_or_path klue/roberta-large \
+--per_device_train_batch_size 32 \
+--per_device_eval_batch_size 32 \
+--gradient_accumulation_steps 4 \
+--num_train_epochs 5 \
 --weight_decay 0.01 \
 --warmup_ratio 0.1 \
 --learning_rate 2e-5 \
---eval_steps 500 \
---logging_steps 500 \
+--eval_steps 100 \
+--logging_steps 100 \
+--save_steps=1000 \
 --load_best_model_at_end \
---evaluation_strategy "steps" \
---wandb_entity \
---wandb_project "test" \
---wandb_name "test"
+--evaluation_strategy steps \
+--wandb_entity  \
+--wandb_project test \
+--wandb_name test
