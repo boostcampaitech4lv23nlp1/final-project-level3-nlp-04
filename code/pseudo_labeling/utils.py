5275ba5233d8cc9d05669c59d79a9b07148b4325
YeRyeongLee <yyi06011@khu.ac.kr>
2023-01-16 09:45:16 +0000

    Fix: fix id2label and label2id

--- code/pseudo_labeling/utils.py
+++ code/pseudo_labeling/utils.py
@@ -12,10 +12,10 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 
 logger = logging.getLogger(__name__)
 
-label2id = {'중립': 0, '행복': 1, '당황': 2,
-            '분노': 3, '불안': 4, '슬픔': 5, '혐오': 6}
+label_list = ['기쁨(행복한)', '고마운', '설레는(기대하는)', '사랑하는', '뿌듯한', '즐거운(신나는)', '일상적인', '놀란', '다짐하는', '슬픔(우울한)', '힘듦(지침)', '짜증남', '걱정스러운(불안한)', '후회하는']
 
-id2label = {idx: token for token, idx in label2id.items()}
+id2label = {id: label for id, label in enumerate(label_list)}
+label2id = {label: id for id, label in enumerate(label_list)}
 
 
 def get_last_checkpoint(args: Arguments, training_args: TrainingArguments) -> Tuple[Any, int]:
