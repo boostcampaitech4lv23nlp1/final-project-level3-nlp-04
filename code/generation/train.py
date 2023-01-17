633c70376e700072ee353105df35e9b8424bb599
root <wdg369@gmail.com>
2023-01-17 14:40:01 +0000

    Fix:

--- code/generation/train.py
+++ code/generation/train.py
@@ -6,6 +6,7 @@ import torch
 import wandb
 import logging
 import datasets
+from functools import partial
 
 from transformers import (
     AutoConfig,
@@ -26,7 +27,7 @@ from transformers.optimization import AdamW, get_cosine_schedule_with_warmup
 
 from arguments import Arguments
 from utils import *
-from dataset import Dataset
+from preprocessing import tokenize_func
 
 
 def train():
@@ -54,29 +55,30 @@ def train():
 
     # config, tokenizer, model
     config = AutoConfig.from_pretrained(args.model_name_or_path)
-    
-    config.label2id = label2id
-    config.id2label = id2label
-    config.num_labels = len(label2id)
-
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_name_or_path, max_length=args.max_seq_length)
+    # config.label2id = label2id
+    # config.id2label = id2label
+    # config.num_labels = len(label2id)
+    # # setting config for decoder
+    # TODO: arg 인자로 넣기.
+    # config.decoder_start_token_id = tokenizer.cls_token_id
+    print("####### cls token: ", tokenizer.cls_token_id)
+    print("####### eos token: ", tokenizer.eos_token_id)
+    config.eos_token_id = tokenizer.sep_token_id
+    config.pad_token_id = tokenizer.pad_token_id
+    config.forced_eos_token_id = tokenizer.eos_token_id
+    config.min_length = 1
+    config.max_length = 128 # TODO: args화
+    config.no_repeat_ngram_size = 2
+    config.early_stopping = True
+    config.length_penalty = 0.0
+    config.num_labels=1
+    config.num_beams = 4
+
+    print("####### config: ", config)
     model = AutoModelForSeq2SeqLM.from_pretrained(
         args.model_name_or_path, config=config)
-
-    # # setting config for decoder
-    model.config.decoder_start_token_id = tokenizer.cls_token_id
-    model.config.eos_token_id = tokenizer.sep_token_id
-    model.config.pad_token_id = tokenizer.pad_token_id
-    model.config.forced_eos_token_id = tokenizer.eos_token_id
-    model.config.min_length = 2
-    model.config.max_length = 10
-    # model.config.config.no_repeat_ngram_size = 3
-    model.config.early_stopping = True
-    model.config.length_penalty = 0.0
-    model.config.num_labels=1
-    model.config.num_beams = 4
-
     ### TODO: rouge score가 0점으로 나오고 있음. compute_metrics를 BLUE로 바꾸거나 rouge score 고치기.
     rouge = datasets.load_metric("rouge")
     def compute_metrics(pred):
@@ -86,12 +88,13 @@ def train():
         if isinstance(pred_ids, tuple):
             pred_ids = pred_ids[0]
             pred_ids = pred_ids.argmax(-1)
-
-        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
+        print('pred_ids: ', pred_ids[:1])
+        print('###pred_len: ', len(pred_ids[0]), len(pred_ids[1]))
+        pred_str = tokenizer.batch_decode(pred_ids, skip_special_tokens=False)
         labels_ids[labels_ids == -100] = tokenizer.pad_token_id
         print('pred_str: ', pred_str[:10])
         label_str = tokenizer.batch_decode(labels_ids, skip_special_tokens=True)
-        # print("label_str: ", label_str[:10])
+        print("label_str: ", label_str[:1])
         rouge_output = rouge.compute(predictions=pred_str, references=label_str, rouge_types=["rouge2"])["rouge2"].mid
         print('### rouge_output: ', rouge_output)
         return {
@@ -100,17 +103,25 @@ def train():
             "rouge2_fmeasure": round(rouge_output.fmeasure, 4),
         }
 
-    
-    
     # 데이터셋
-    train_df = pd.read_csv(args.train_data)
-    eval_df = pd.read_csv(args.eval_data)
-    
-    train_dataset = Dataset(train_df, tokenizer, config)
-    eval_dataset = Dataset(eval_df, tokenizer, config)
+    train_dataset = datasets.load_dataset('csv', data_files=args.train_data, split='train')
+    eval_dataset = datasets.load_dataset('csv', data_files=args.eval_data, split='train')
 
-    data_collator = DataCollatorWithPadding(
-        tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
+    # 필요없는 칼럼을 삭제합니다.
+    train_dataset.remove_columns(['diary'])
+    eval_dataset.remove_columns(['diary'])
+
+    # 데이터셋을 전처리합니다.
+    prepro_fn = partial(tokenize_func, tokenizer=tokenizer, max_input_length=512, max_target_length=128) # TODO: max_len args화
+    tokenized_train_dataset = train_dataset.map(prepro_fn,
+                                                batched=True,
+                                                )
+    tokenized_eval_dataset = eval_dataset.map(prepro_fn,
+                                                batched=True,
+                                                )
+
+    data_collator = DataCollatorForSeq2Seq(
+        tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None, model = model
     )
     
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -119,8 +130,8 @@ def train():
     trainer = Seq2SeqTrainer(
         model=model,
         args=training_args,               
-        train_dataset=train_dataset,        
-        eval_dataset=eval_dataset,          
+        train_dataset=tokenized_train_dataset,        
+        eval_dataset=tokenized_eval_dataset,          
         data_collator=data_collator,
         tokenizer=tokenizer,
         compute_metrics=compute_metrics,
